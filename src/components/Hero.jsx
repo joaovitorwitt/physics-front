519f16e29442c79cbc7c9ea786958836bef8176b
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-12-29 23:09:52 -0300

    wraping up black hole

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,45 +1,21 @@
-import { useState } from "react";
 import "../assets/css/Hero.css";
-import { useEffect } from "react";
 
 export default function Hero() {
-  const [blackholeAlphaValue, setBlackholeAlphaValue] = useState(0.97);
-  const [decreasingValue, setDecreasingValue] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (decreasingValue) {
-        setBlackholeAlphaValue((prevAlpha) => prevAlpha - 0.01);
-
-        if (blackholeAlphaValue <= 0.2) {
-          setDecreasingValue(false);
-        }
-      } else {
-        setBlackholeAlphaValue((prevAlpha) => prevAlpha + 0.01);
-
-        if (blackholeAlphaValue >= 0.97) {
-          setDecreasingValue(true);
-        }
-      }
-    }, 35);
-
-    return () => clearInterval(interval);
-  }, [blackholeAlphaValue, decreasingValue]);
-
   return (
     <div className="hero-wrapper">
-      <div
-        className="black-hole-wrapper"
-        // style={{
-        //   boxShadow: `0px 2px 200px 12px rgba(8, 0, 230, ${blackholeAlphaValue})`,
-        // }}
-      >
-        <div className="inner-blackhole"></div>
+      <div className="black-hole-wrapper">
+        <div className="doppler-beaming"></div>
+        <div className="photon-ring"></div>
+        <div className="accretion-disk"></div>
+        <div className="backdrop"></div>
+        <div className="shadow"></div>
       </div>
-      <h1 className="main-title">
-        Elevate your physics knownledge to the next level
-      </h1>
-      <p className="main-description">Learn physics in the right way </p>
+      <div className="hero-cta">
+        <h1 className="main-title">
+          Elevate your physics knownledge to the next level
+        </h1>
+        <p className="main-description">Learn physics in the right way </p>
+      </div>
     </div>
   );
 }
