e1a8f1e31141da6483a0982be9763706af49d0a4
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-02-08 13:30:36 -0300

    refactoring homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,75 +1,11 @@
 import "../assets/css/Header.css";
 import { Link } from "react-router-dom";
-import Logo from "../assets/images/logo192.png";
+import Logo from "../assets/images/physics-platform-logo.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 export default function Header() {
-  const [isMobileMenuActive, setIsMobileMenuActive] = useState(false);
-  const [mobileMenuIcon, setMobileMenuIcon] = useState(faBars);
-
-  function toggleMobileMenu() {
-    setIsMobileMenuActive((prevState) => !prevState);
-    console.log(isMobileMenuActive);
-
-    if (isMobileMenuActive) {
-      setMobileMenuIcon(faBars);
-    } else {
-      setMobileMenuIcon(faClose);
-    }
-  }
-
-  return (
-    <>
-      <header className="header">
-        <nav className="navbar container">
-          <div className="navbar-logo">
-            <img src={Logo} alt="" />
-          </div>
-          <div className="navbar-actions">
-            <Link to={"/login"}>
-              <button className="btn login-btn">Start Learning</button>
-            </Link>
-            <button className="display-navbar-menu" onClick={toggleMobileMenu}>
-              {/* if mobile menu is active change the icon to the close */}
-              <FontAwesomeIcon icon={mobileMenuIcon} />
-            </button>
-          </div>
-        </nav>
-      </header>
-      <div
-        className={`navbar-mobile-menu ${isMobileMenuActive ? "active" : ""}`}
-      >
-        <ul>
-          <li>
-            <Link to={"/"} className="navbar-item-mobile">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to={"/"} className="navbar-item-mobile">
-              Features
-            </Link>
-          </li>
-          <li>
-            <Link to={"/"} className="navbar-item-mobile">
-              Stuff
-            </Link>
-          </li>
-          <li>
-            <Link to={"/"} className="navbar-item-mobile">
-              Stuff
-            </Link>
-          </li>
-          <li>
-            <Link to={"/"} className="navbar-item-mobile">
-              Stuff
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+  return <>this is the header</>;
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,5 @@
 import "../assets/css/Hero.css";
 
 export default function Hero() {
-  return (
-    <div className="hero-wrapper">
-      <div>
-        <div className="hero-cta">
-          <h1 className="main-title">Elevate your physics knownledge</h1>
-          {/* <p className="main-description">Learn physics in the right way </p> */}
-        </div>
-      </div>
-    </div>
-  );
+  return <>this is the hero</>;
 }
